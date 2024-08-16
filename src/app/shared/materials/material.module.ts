bca21af547910fc591d3f1f9ef0f7f0bc73b117c
danilofrancisquete <danilofrancisquete@hotmail.com>
2024-08-16 16:49:24 -0300

    modules

---
+++ src/app/shared/materials/material.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS} from '@angular/material/snack-bar';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
+
+
+
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
+  declarations: [],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {appearance:'outline', floatLabel:'always'}
+    },
+    {
+      provide: MAT_DATE_LOCALE,useValue: 'pt-br'
+    },
+    {
+      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+      useValue: {
+        duration: 2500,
+        horizontalPosition: 'end',
+        verticalPosition:'top',
+      }
+    }
+  ]
+
+})
+export class MaterialModule { }
