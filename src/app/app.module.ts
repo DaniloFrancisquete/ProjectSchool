dfead5383fe4a1afcfa384f5a8ae1c2c7aa95b5f
danilofrancisquete <danilofrancisquete@hotmail.com>
2024-08-22 17:33:14 -0300

    toolbar-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/materials/material.module';
-
+import { ToolbarMenuComponent } from '@app/shared/components/toolbar-menu/toolbar-menu.component';
 @NgModule({
   declarations: [
     AppComponent,
+    ToolbarMenuComponent
 
   ],
   imports: [
