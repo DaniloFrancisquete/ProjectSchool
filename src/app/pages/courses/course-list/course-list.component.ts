da5e8bf22b2a53261dcee0e32b259d3e1dc6a54a
danilofrancisquete <danilofrancisquete@hotmail.com>
2024-09-11 16:07:36 -0300

    finalizado

--- src/app/pages/courses/course-list/course-list.component.ts
+++ src/app/pages/courses/course-list/course-list.component.ts
@@ -1,10 +1,104 @@
-import { Component } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { PageEvent } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CoursesService } from '@app/services/coures.service';
+import { Category, Course } from '@app/shared/models/course';
+import { catchError, debounceTime, EMPTY, Observable, Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
-  styleUrls: [ './course-list.component.scss']
+  styleUrls: ['./course-list.component.scss'],
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
 
+  public courseList: Course[] = [];
+  private courseService = inject(CoursesService);
+  private fb = inject(FormBuilder);
+  public snackbar = inject (MatSnackBar);
+  public categoryValue = Object.values(Category);
+  public form!: FormGroup;
+  public courseData!: Observable<any>
+
+  totalCount: number = 0;
+  currentPage: number = 1;
+  pageSize: number = 5;
+
+  private validation(): void {
+    this.form = this.fb.group({
+      category: [''],
+      search: [''],
+    });
+  }
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  ngOnInit(): void {
+    this.validation();
+    this.form.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe((value) => {
+      if (value) {
+        this.getCourses(
+          this.currentPage,
+          this.pageSize,
+          this.f.category.value ?? '',
+          this.f.search.value ?? ''
+        );
+      }
+    });
+    this.getCourses(1, 5, '', '');
+  }
+
+
+
+  public doSearch(): void {
+    console.log('Categoria:', this.f.category.value, 'Search:', this.f.search.value); // Verificar os valores
+    this.getCourses(
+      this.currentPage,
+      this.pageSize,
+      this.f.category.value ?? '',
+      this.f.search.value ?? ''
+    );
+  }
+
+  public getCourses(
+    currentPage: number,
+    pageSize: number,
+    category: string,
+    search: string
+  ): void {
+    console.log('Chamada API com parâmetros:', { currentPage, pageSize, category, search });
+    this.courseData = this.courseService
+      .get(currentPage, pageSize, category, search)
+      .pipe(
+      tap((response: HttpResponse<any>) => {
+        console.log('Resposta da API:', response.body); // Verificar a resposta
+        this.courseList = response.body as Course[];
+        let totalCount = response.headers.get('X-Total-Count');
+        this.totalCount = totalCount ? Number(totalCount) : 0;
+      }),
+      catchError((err: string) => {
+        this.snackbar.open(err,' Close', {
+          duration: 5000
+        });
+        return EMPTY;
+      })
+    )
+  }
+
+  public handlePageEvent(e: PageEvent): void {
+    this.currentPage = (e.pageIndex + 1);
+    this.pageSize = e.pageSize;
+    this.getCourses(
+      this.currentPage,
+      this.pageSize,
+      this.f.category.value ?? '',
+      this.f.search.value ?? ''
+    );
+  }
 }
--- src/app/services/coures.service.ts
+++ src/app/services/coures.service.ts
@@ -1,36 +1,77 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { environment } from '@app/environments/environments';
+
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
 import { Course } from '@app/shared/models/course';
-import { Observable } from 'rxjs';
+import { catchError, Observable, take, throwError } from 'rxjs';
+import { environment } from '@app/environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CouresService {
-
-  baseURL = `${environment.apiURL}courses`
+export class CoursesService {
+  baseURL = `${environment.apiURL}courses`;
   private http = inject(HttpClient);
 
-public get(id: number): Observable<Course[]> {
-  return this.http.get<Course[]>(`${this.baseURL}`)
-}
+  public get(
+    currentPage: number,
+    pageSize: number,
+    category: string,
+    search: string
+    ): Observable<HttpResponse<any>> {
+    let url = `${this.baseURL}?_page=${currentPage}&_limit=${pageSize}`
+    if(category)
+      url = `${url}&category=${category}`
 
-public getById(id: number): Observable<Course[]> {
-  return this.http.get<Course[]>(`${this.baseURL}/${id}`)
-}
+    if(search)
+      url = `${url}&q=${search}`
+    return this.http.get<Course[]>(`${url}`, { observe: 'response'})
+    .pipe(
+      take(1),
+      catchError(this.handleError)
+    )
+  }
 
-public post(course: Course): Observable<Course[]> {
-  return this.http.post<Course[]>(`${this.baseURL}`, course)
-}
+  public getById(id: number): Observable<Course[]> {
+    return this.http.get<Course[]>(`${this.baseURL}/${id}`)
+    .pipe(
+      take(1),
+      catchError(this.handleError)
+    )
+  }
 
-public put(id: number, course: Course): Observable<Course[]> {
-  return this.http.put<Course[]>(`${this.baseURL}/${id}`, course)
-}
+  public post(course: Course): Observable<Course[]> {
+    return this.http.post<Course[]>(`${this.baseURL}`, course)
+    .pipe(
+      take(1),
+      catchError(this.handleError)
+    )
+  }
+
+  public put(id: number, course: Course): Observable<Course[]> {
+    return this.http.put<Course[]>(`${this.baseURL}/${id}`, course)
+    .pipe(
+      take(1),
+      catchError(this.handleError)
+    )
+  }
+
+  public delete(id: number): Observable<Course[]> {
+    return this.http.delete<Course[]>(`${this.baseURL}/${id}`)
+    .pipe(
+      take(1),
+      catchError(this.handleError)
+    )
+  }
 
 
-public delete(id: number): Observable<Course[]> {
-  return this.http.delete<Course[]>(`${this.baseURL}/${id}`)
-}
-
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    let errorMessage:string;
+    if(err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurrend: ${err.error.message}`
+    } else {
+      errorMessage = `Backend returned code: ${err.status}: ${err.message}`
+    }
+    console.error(err);
+    return throwError(() => errorMessage)
+  }
 }
